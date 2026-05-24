933068fea4d6f5ccbc182cda903969a1b8f81a6f
Moeen-Islam <moeenislam809@gmail.com>
2026-05-25 02:47:26 +0600

    Project added

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,8 +4,9 @@ export const skills = [
 
 export const projects = [
   {title:'KeenKeeper',cat:'Frontend',img:'./keenkeeper.png',desc:'Analytics dashboard with auth, cart, payment-ready UI and admin tools.',tech:['Next.js','Tailwind','App Router'],live:'https://keen-keeper-moeen.vercel.app/',github:'https://github.com/Moeen-Islam/PH-Assignment-7-KeenKeeper'},
+  {title:'PawNest Adoption Platform',cat:'FullStack',img:'./pawnest.png',desc:'PawNest is a full-stack web platform designed to connect loving families with pets available for adoption. Users can browse pets, view detailed profiles, submit adoption requests, and manage their requests.',tech:['Next.js','Tailwind','App Router','Mongodb','JsonWebtoken'],live:'https://paw-nest-client.vercel.app/',github:'https://github.com/Moeen-Islam/PH-Assignment-9-PawNest-Client'},
   {title:'BookNest — Online Book Borrowing Platform',cat:'FullStack',img:'./Booknest.png',desc:'Premium landing page with animations, pricing, FAQ and conversion sections.',tech:['Next.js','Tailwind','MongoDB','BetterAuth'],live:'https://book-nest-moeen-islam.vercel.app/',github:'https://github.com/Moeen-Islam/PH-Assignment-8-BookNest'},
-  {title:'Portfolio Website',cat:'Frontend',img:'./portfolio.png',desc:'Responsive personal portfolio with glassmorphism and elegant micro-interactions.',tech:['React','Tailwind'],live:'#',github:'https://github.com/Moeen-Islam/Moeen-Islam-Personal-Portfolio'}
+  {title:'Portfolio Website',cat:'Frontend',img:'./portfolio.png',desc:'Responsive personal portfolio with glassmorphism and elegant micro-interactions.',tech:['React','Tailwind'],live:'https://moeen-islam-dev-portfolio.vercel.app/',github:'https://github.com/Moeen-Islam/Moeen-Islam-Personal-Portfolio'}
 ];
 
 export const timeline = [
